9db6e531a6e1a26575977cc84f5d1328f6145b53
HyeChang <a4076a4076@gmail.com>
2026-05-18 17:43:23 +0900

    [메인 UI] 메인 UI 기초 틀 완성

---
+++ Assets/Scripts/DN_UI/MainUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MainUI : MonoBehaviour
+{
+    [SerializeField] private DaniTechUIButton Button_Exit;
+
+
+
+
+    private void OnEnable()
+    {
+        Button_Exit.BindOnClickButtonEvent(OnClick_Exit);
+    }
+
+    private void OnClick_Exit()
+    {
+        DaniTechUIManager.Instance.OpenPopupUI();
+    }
+
+
+
+
+}
