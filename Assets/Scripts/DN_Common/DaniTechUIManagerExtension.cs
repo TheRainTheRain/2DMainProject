de0dc9e6bcdc99ea296f41e39980f17aab391348
HyeChang <a4076a4076@gmail.com>
2026-05-18 13:55:46 +0900

    [오프닝 UI] 프리팹과 코드작업 완료

--- Assets/Scripts/DN_Common/DaniTechUIManagerExtension.cs
+++ Assets/Scripts/DN_Common/DaniTechUIManagerExtension.cs
@@ -18,7 +18,8 @@ public enum DaniTechUIType
     DNInventory,
     DNLoadingUI,
     DNDialogueUI,
-    DNInfoBookUI
+    DNInfoBookUI,
+    OpeningUI
 }
 
 public static class DaniTechUIManagerExtension
@@ -35,7 +36,7 @@ public static class DaniTechUIManagerExtension
 
     public static void ShowStartupUIOnGameStart(this DaniTechUIManager uiManager)
     {
-        uiManager.OpenLoadingUI();
+        uiManager.OpenContentUI(DaniTechUIType.OpeningUI);
         uiManager.OpenUI(DaniTechUIRootType.MainUI, DaniTechUIType.DNMainUI);
         // 게임 로비 UI를 여기서 오픈해주자 -> uiManager.
         // MainUI도
---
+++ Assets/Scripts/DN_UI/OpeningUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Opening : DaniTechUIBase
+{
+    [SerializeField] private DaniTechUIButton Button_Gamestart;
+    [SerializeField] private DaniTechUIButton Button_Quit;
+
+    private void OnEnable()
+    {
+        Button_Gamestart.BindOnClickButtonEvent(OnClick_GameStart);
+        Button_Quit.BindOnClickButtonEvent(OnClick_Quit);
+    }
+
+    private void OnClick_GameStart()
+    {
+        DaniTechUIManager.Instance.CloseContentUI(DaniTechUIType.OpeningUI);
+    }
+
+    private void OnClick_Quit()
+    {
+        DaniTechGameManager.Inst.SaveAndEndGame();
+    }
+}
