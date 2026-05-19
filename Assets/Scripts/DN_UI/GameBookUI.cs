c5f9b76666386760f5b586fe2fc72ea29266ec92
HyeChang <a4076a4076@gmail.com>
2026-05-19 16:10:16 +0900

    [GameBookUI] 칵테일 도감 기초작업

---
+++ Assets/Scripts/DN_UI/GameBookUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameBookUI : DaniTechUIBase
+{
+    [SerializeField] private DaniTechUIButton Button_ChoiceTaste;
+    [SerializeField] private DaniTechUIButton Button_ChoiceFrom;
+
+    private void OnEnable()
+    {
+        Button_ChoiceTaste.BindOnClickButtonEvent(OnClick_Taste);
+        Button_ChoiceFrom.BindOnClickButtonEvent(OnClick_From);
+    }
+
+    public void OnClick_Taste()
+    {
+
+    }
+
+    public void OnClick_From()
+    {
+
+    }
+
+
+
+
+}
