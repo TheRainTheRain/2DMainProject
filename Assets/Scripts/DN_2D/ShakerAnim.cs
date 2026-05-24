ed37b2f423eac1aeba3a0092d6f21f87ac592282
HyeChang <a4076a4076@gmail.com>
2026-05-24 16:12:49 +0900

    [AnimController] 쉐이커 애니메이터 제어하는 컴포넌트 작성

---
+++ Assets/Scripts/DN_2D/ShakerAnim.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public enum ShakerAnimState
+{
+    None = 0,
+    Idle,
+    Shake
+}
+
+public class ShakerAnim : MonoBehaviour
+{
+    [SerializeField] private Animator Animator_Shaker;
+
+    private ShakerAnimState _currentState;
+
+    public void SteShakerAnimState(ShakerAnimState newState)
+    {
+        _currentState = newState;
+
+        switch (_currentState)
+        {
+            case ShakerAnimState.Idle:
+                ResetAllAnimParameters();
+                break;
+            case ShakerAnimState.Shake:
+                Animator_Shaker.SetBool("IsShake", true);
+                break;
+        }
+    }
+
+    private void ResetAllAnimParameters()
+    {
+        Animator_Shaker.SetBool("IsShake", false);
+    }
+}
