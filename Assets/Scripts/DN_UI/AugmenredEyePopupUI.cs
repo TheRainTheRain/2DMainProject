c29fa9b977ea29eca13671e5081dbcee3f217af5
HyeChang <a4076a4076@gmail.com>
2026-05-30 01:53:01 +0900

    [AugmenredEyePopupUI] 매니저를 사용한 켯다 꺼졌다 하는 버튼 코드 작성

--- Assets/Scripts/DN_UI/AugmenredEyePopupUI.cs
+++ Assets/Scripts/DN_UI/AugmenredEyePopupUI.cs
@@ -2,11 +2,23 @@
 
 public class AugmenredEyePopupUI : DaniTechUIBase
 {
+    [SerializeField] private DaniTechUIButton Button_Back;
+    [SerializeField] private DaniTechUIButton Button_Home;
 
+    private void OnEnable()
+    {
+        Button_Back.BindOnClickButtonEvent(OnClick_Back);
+        Button_Home.BindOnClickButtonEvent(OnClick_Home);
+    }
 
+    private void OnClick_Back()
+    {
+        DaniTechUIManager.Instance.ClosePopupUI(DaniTechUIType.AugmenredEyePopupUI);
+    }
 
-
-
-
+    private void OnClick_Home()
+    {
+        DaniTechUIManager.Instance.ClosePopupUI(DaniTechUIType.AugmenredEyePopupUI);
+    }
 
 }
